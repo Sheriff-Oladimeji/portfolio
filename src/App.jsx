06be7daf00418acbcf4fab22bb7f01f0d69c7f54
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-09-16 10:36:39 +0100

    feat: add vercel analytics

--- src/App.jsx
+++ src/App.jsx
@@ -4,19 +4,21 @@ import Footer from "./components/Footer"
 import Header from "./components/Header"
 import Navbar from "./components/Navbar"
 import Projects from "./components/Projects"
+import { Analytics } from "@vercel/analytics/react";
 
 
 const App = () => {
   return (
-    <div className='bg-darkBg relative flex flex-col justify-between font-inter text-white'>
+    <div className="bg-darkBg relative flex flex-col justify-between font-inter text-white">
       <Navbar />
       <Header />
       <About />
       <Projects />
-      <Contact/>
-      <Footer/>
+      <Contact />
+      <Footer />
+      <Analytics />
     </div>
-  )
+  );
 }
 
 export default App
