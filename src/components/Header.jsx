c2d3093d98ed4721043f218c2900bb0814e4eeae
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-10-03 18:48:25 +0100

    feat: update resume

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     <header className="container" name="home">
       <div className="flex flex-col h-full pt-20 w-full  gap-6">
         <span className="text-green text-lg font-semibold">Hi, my name is</span>
-        <h1 className="text-4xl sm:text-5xl md:text-7xl font-semibold text-bold  tracking-widest ">
+        <h1 className="text-3xl sm:text-4xl md:text-6xl lg:text-7xl  font-semibold text-bold  tracking-widest ">
           Sheriff Oladimeji
         </h1>
         <h2 className="text-3xl sm:text-4xl md:text-6xl font-semibold text-gray  tracking-wider ">
@@ -18,14 +18,14 @@ const Header = () => {
         </p>
         <div className="flex items-center gap-8">
           <a
-            href={resume}
+            href="https://drive.google.com/file/d/1-uRe8asCDWo-3tI5ptk_g5IOUPp0F2pj/view?usp=sharing"
             download={true}
             className="button py-4 px-4"
+            target="_blank"
+            rel="noreferrer"
           >
             View my resume
           </a>
-          
-          
         </div>
       </div>
     </header>
