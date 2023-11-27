773cab7d8a2486feed15388785983cdaff9129c6
sheriff Oladimeji <dimejiademola5@gmail.com>
2023-11-27 14:57:28 +0100

    fix: improve header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -12,13 +12,11 @@ const Header = () => {
           Sheriff Oladimeji
         </h1>
         <h2 className="text-3xl sm:text-4xl md:text-6xl font-semibold text-gray  tracking-wider ">
-          I'm a Frontend Developer
+          I'm a Fullstack Developer
         </h2>
 
         <p className="text-gray max-w-md md:max-w-3xl  text-lg ">
-          A passionate frontend developer dedicated to crafting captivating
-          digital experiences, I love the thrill of bringing ideas to life
-          through clean, functional design and code.
+          Crafting captivating web experiences, from concept to code.
         </p>
         <div className="flex items-center gap-8">
           <a
