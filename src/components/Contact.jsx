587342df420e25c5607d54d2bfdbcc8934a64725
sheriff Oladimeji <dimejiademola5@gmail.com>
2024-01-05 23:13:33 +0100

    fix: modify contact compononet

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -4,13 +4,13 @@ const sidelinks = [
   {
     id: 1,
     name: "Github",
-    icon: <FaGithub size={30} />,
+    icon: <FaGithub size={40} />,
     url: "https://github.com/Sheriff-Oladimeji",
   },
   {
     id: 2,
     name: "Linkedin",
-    icon: <FaLinkedin size={30} />,
+    icon: <FaLinkedin size={40} />,
     url: "https://www.linkedin.com/in/sheriff-oladimeji-022362255/",
   },
 ];
@@ -23,33 +23,24 @@ const Contact = () => {
       <h1 className="text-4xl sm:text-5xl font-bold mb-2 text-bold ">
         Contact Me
       </h1>
-      <p className="text-center">
-        Got a project we could work on together? I'm currently looking for new
-        opportunities.
-      </p>
+      <p className="text-center text-gray text-xl">Got a project we could work on together?</p>
 
-      <a
-        href="mailto:dimejiademola5@gmail.com"
-        className="button py-4 px-4 flex items-center gap-3"
-      >
-        Send An Email <HiOutlineMail size={30} />
-      </a>
-      <div className="flex items-center gap-6">
+      <div className="flex items-center gap-6 mt-6 flex-wrap">
+        <a
+          href="mailto:dimejiademola5@gmail.com"
+          className={`h-20 w-20  rounded-full  outline-none font-medium flex  items-center justify-center text-[#5ADFCB] py-1 px-2 bg-[#122B39] text-sm `}
+        >
+          <HiOutlineMail size={40} />
+        </a>
         {sidelinks.map((link) => (
           <a
             key={link.id}
             href={link.id === 3 ? "mailto:" + link.url : link.url}
             target="_blank"
             rel="noopener noreferrer"
-            className={` flex   gap-4 items-center p-3 rounded-md ${
-              link.id === 1
-                ? "bg-black"
-                : link.id === 2
-                ? "bg-blue-600"
-                : "bg-red-700"
-            }`}
+            className={`h-20 w-20  rounded-full  outline-none font-medium flex  items-center justify-center text-[#5ADFCB] py-1 px-2 bg-[#122B39] text-sm `}
           >
-            {link.name} {link.icon}
+            {link.icon}
           </a>
         ))}
       </div>
